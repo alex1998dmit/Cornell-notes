0d0f6fbfc48c79b033d3da9ae0dfa551e0a48075
alex1998dmit <alex1998dmit@gmail.com>
2019-03-21 16:07:01 +0300

    Adding migrations

---
+++ database/migrations/2019_03_21_125624_create_notes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->bigIncrements('id_note');
+            $table->integer('id_subject');
+            $table->boolean('isOpen');
+            $table->text('leftColumn');
+            $table->text('rightColumn');
+            $table->text('botterColumn');
+            $table->string('theme');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
---
+++ database/migrations/2019_03_21_130233_create_users_notes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_notes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_user');
+            $table->integer('id_note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_note');
+    }
+}
