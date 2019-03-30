87f644b804b55cad639a9dc490fa8385b80de916
alex1998dmit <alex1998dmit@gmail.com>
2019-03-30 10:30:25 +0300

    Add displaying validations  errors in edit note

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+    @if(count($errors))
+    <ul class="alert alert-danger">
+        @foreach($errors->all() as $error)
+            <li>{{$error}}</li>
+        @endforeach
+    </ul>
+    @endif
     <form action="{{ route('note.update', ['id' => $note->id]) }}" method="post">
         {{ csrf_field() }}
         <div class="row">
