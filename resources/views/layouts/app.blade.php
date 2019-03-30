115d6ff9bf0e0d3f132297e3eef6c0e055c6da2a
alex1998dmit <alex1998dmit@gmail.com>
2019-03-30 14:40:19 +0300

    Fix edit validations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Выйти
                                         </a>
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -45,7 +45,7 @@
                         <div class="row col-12 align-items-center justify-content-between pt-2 pb-2 mb-2 lection-card">
                             <a href="#" class="block-link"></a>
                             <span class="col-7 lection-card__title">{{ $note->theme }}</span>
-                            <span class="col-3 lection-card__date">{{ $note->updated_at}}</span>
+                            <span class="col-3 lection-card__date">{{ date('d-m-Y', strtotime($note->updated_at))}}</span>
                             <div class="col-auto justify-self-end lection-card__buttons">
                                 <a class="btn btn-info lection-card__btn lection-card__edit" href="{{ route('note.edit', ['id' => $note->id])}}">Edit</a>
                                 <a class="btn btn-danger lection-card__btn lection-card__delete" href="{{ route('note.delete', ['id' => $note->id]) }}">Delete</a>
