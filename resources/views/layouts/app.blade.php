d736a4694949369e01ee4a3bf501b54d53445e0d
RukkiesMan <pavelekseev@gmail.com>
2019-03-31 18:36:22 +0300

    Fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,10 +38,7 @@
                     @if(Auth::user())
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('note.create') }}">Create note</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('user', Auth::user()->id) }}">Profile</a>
+                            <a class="nav-link" href="{{ route('note.create') }}">Создать лекцию</a>
                         </li>
                     </ul>
                     @endif
@@ -49,36 +46,34 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
+                        <li class="pr-2"><a href="{{ route('login') }}">Вход</a></li>
+                        <li><a href="{{ route('register') }}">Регистрация</a></li>
                         @else
                         <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    <img src="{{asset('uploads/avatars/'.Auth::user()->avatar)}}" alt="" style="width: 20px; height: 20px; position:absolute; border-radius: 50%;left: 127px;bottom: 3px;">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                            <ul class="dropdown-menu">
+                                <li class="text-center">
+                                    <a href="{{ route('user', ['id' => Auth::user()->id]) }}">
+                                        Профиль
+                                    </a>
+                                </li>
+                                <div class="dropdown-divider"></div>
+                                <li class="text-center">
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                        Выйти
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
 
-
-                                <ul class="dropdown-menu">
-                                    <li class="text-center">
-                                        <a href="{{ route('user', ['id' => Auth::user()->id]) }}">
-                                            Профиль
-                                        </a>
-                                    </li>
-                                    <div class="dropdown-divider"></div>
-                                    <li class="text-center">
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Выйти
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-
-                                </ul>
-                            </li>
+                            </ul>
+                            <a href="#" class="dropdown-toggle menu" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                {{ Auth::user()->name }}
+                                <img class="menu__avatar ml-2 mr-2" src="{{asset('uploads/avatars/'.Auth::user()->avatar)}}" alt="avatar">                            
+                            </a>
+                        </li>
                         @endguest
                     </ul>
                 </div>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('content')
-<div class="row" style="border: 1px solid grey; border-radius: 1%; padding: 10px;">
+
+
+<!-- <div class="row" style="border: 1px solid grey; border-radius: 1%; padding: 10px;">
     <div class="col-md-9">
         <p>Имя: {{ $user->name }}</p>
         <p>Email: {{ $user->email }}</p>
@@ -10,49 +12,54 @@
     <div class="col-md-3">
         <img src="{{asset('uploads/avatars/'.Auth::user()->avatar)}}" alt="" style="width:150px; height: 150px;border-radius: 50%;">
     </div>
+</div> -->
+<div class="col-sm-4">
+
 </div>
-@if(count($notes) == 0)
-    <div class="lection mt-5 text-center">
-        <h4>Вы не добавили ни одной лекции, хотите добавить?</h4>
-        <div class="row">
-            <div class="col pt-3">
-            <a href="{{ route('note.create') }}" class="btn btn-success">Добавить лекцию</a>
+<div class="col-sm-8">
+    @if(count($notes) == 0)
+        <div class="lection mt-5 text-center">
+            <h4>Вы не добавили ни одной лекции, хотите добавить?</h4>
+            <div class="row">
+                <div class="col pt-3">
+                <a href="{{ route('note.create') }}" class="btn btn-success">Добавить лекцию</a>
+                </div>
             </div>
         </div>
-    </div>
-@else
-    @foreach($subjects as $subject)
-    <div class="lection mt-5">
-        <div class="row">
-        <div class="col">
-            <a class="lection__more d-flex text-dark" data-toggle="collapse" href="#subject{{ $subject->id }}" role="button" aria-expanded="false" aria-controls="subject{{ $subject->id }}">
-            <i class="fas fa-caret-down mt-2 text-dark dropdown-arrow pr-2"></i>
-            <h2 class="lection__theme">{{ $subject->name }}</h2>
-            </a>
-        </div>
-        <div class="col col-auto">
-            <a href="#" class="btn btn-success">Добавить лекцию</a>
-        </div>
-        <div class="w-100"></div>
-        <hr class="col">
-        </div>
-        <div class="row collapse" id="subject{{ $subject->id }}">
-        @foreach ($notes as $note)
-            @if($note->subject_id == $subject->id)
-                <div class="row col-12 align-items-center justify-content-between pt-2 pb-2 mb-2 lection-card">
-                    <a href="#" class="block-link"></a>
-                    <span class="col-7 lection-card__title">{{ $note->theme }}</span>
-                    <span class="col-3 lection-card__date">{{ $note->updated_at}}</span>
-                    <div class="col-auto justify-self-end lection-card__buttons">
-                        <a class="btn btn-info lection-card__btn lection-card__edit" href="{{ route('note.edit', ['id' => $note->id])}}">Edit</a>
-                        <a class="btn btn-danger lection-card__btn lection-card__delete" href="{{ route('note.delete', ['id' => $note->id]) }}">Delete</a>
+    @else
+        @foreach($subjects as $subject)
+        <div class="lection mt-5">
+            <div class="row">
+            <div class="col">
+                <a class="lection__more d-flex text-dark" data-toggle="collapse" href="#subject{{ $subject->id }}" role="button" aria-expanded="false" aria-controls="subject{{ $subject->id }}">
+                <i class="fas fa-caret-down mt-2 text-dark dropdown-arrow pr-2"></i>
+                <h2 class="lection__theme">{{ $subject->name }}</h2>
+                </a>
+            </div>
+            <div class="col col-auto">
+                <a href="#" class="btn btn-success">Добавить лекцию</a>
+            </div>
+            <div class="w-100"></div>
+            <hr class="col">
+            </div>
+            <div class="row collapse" id="subject{{ $subject->id }}">
+            @foreach ($notes as $note)
+                @if($note->subject_id == $subject->id)
+                    <div class="row col-12 align-items-center justify-content-between pt-2 pb-2 mb-2 lection-card">
+                        <a href="#" class="block-link"></a>
+                        <span class="col-5 lection-card__title">{{ $note->theme }}</span>
+                        <span class="col-2 lection-card__date">{{ $note->updated_at}}</span>
+                        <div class="col-auto justify-self-end lection-card__buttons">
+                            <a class="btn btn-info lection-card__btn lection-card__edit" href="{{ route('note.edit', ['id' => $note->id])}}">Edit</a>
+                            <a class="btn btn-danger lection-card__btn lection-card__delete" href="{{ route('note.delete', ['id' => $note->id]) }}">Delete</a>
+                        </div>
                     </div>
-                </div>
-            @endif
-        @endforeach
+                @endif
+            @endforeach
+            </div>
         </div>
-    </div>
-    @endforeach
+        @endforeach
 
-@endif
+    @endif
+</div>
 @endsection
