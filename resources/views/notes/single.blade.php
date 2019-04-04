cc023959a0235e21a32b8737f305aeb3522917c4
RukkiesMan <pavelekseev@gmail.com>
2019-04-05 02:36:29 +0300

    Update single note page

--- resources/views/notes/single.blade.php
+++ resources/views/notes/single.blade.php
@@ -1,32 +1,24 @@
 @extends('layouts.app')
-
-    @section('content')
-    <a class="btn btn-danger lection-card__delete delete-button text-white" data-id={{ $note->id }}>Удалить</a>
-    <div>
-        <div class="row">
-            <div class="col-md-12">
-                {{ $note->subject->name }}
+@section('content')
+    <main role="main">
+        <h1 class="display-4 mt-4 mb-0">{{ $note->theme }}</h1>
+        <a href="{{ route('subject', ['id' => $note->subject->id]) }}" class="lead d-block mb-4">{{ $note->subject->name }}</a>
+        <form action="{{ route('note.store') }}" method="post">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="">Основные мысли</label>
+                <textarea class="form-control" name="leftColumn" id="leftColumn" rows="10" readonly>{{$note->leftColumn}}</textarea>
             </div>
-        </div>
-        <div class="row">
-                <div class="col-md-12">
-                    {{ $note->theme }}
-                </div>
+            <div class="form-group">
+                <label for="">Лекция</label>
+                <textarea class="form-control" name="rightColumn" id="rightColumn" rows="10" readonly>{{$note->rightColumn}}</textarea>
             </div>
-        <div class="row">
-            <div class="col-md-12">
-                {{ $note->leftColumn}}
+            <div class="form-group">
+                <label for="">Резюме</label>
+                <textarea class="form-control" name="bottemColumn" id="bottemColumn" rows="10" readonly>{{$note->bottembColumn}}</textarea>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                {{ $note->rightColumn }}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                {{ $note->bottemColumn }}
-            </div>
-        </div>
-    </div>
-    @endsection
+            <a class="btn btn-primary" href="{{ route('note.edit', ['id' => $note->id])}}" role="button">Редактировать</a>
+            <a class="btn btn-danger text-white" href="{{ route('note.trash', ['id' => $note->id]) }}}">Удалить</a>
+        </form>
+    </main>
+@endsection
