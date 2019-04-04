f89aafac420ee7a92cc02fe4894f8b1aeee5796a
RukkiesMan <pavelekseev@gmail.com>
2019-04-04 22:40:12 +0300

    Add delete button

--- resources/views/notes/index.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('layouts.app')
-@section('content')
-  <a class="btn btn-danger lection-card__btn lection-card__delete delete-button text-white" data-id={{ $note->id }}>Удалить</a>
-@endsection
--- resources/views/notes/single.blade.php
+++ resources/views/notes/single.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
     @section('content')
-
+    <a class="btn btn-danger lection-card__delete delete-button text-white" data-id={{ $note->id }}>Удалить</a>
     <div>
         <div class="row">
             <div class="col-md-12">
